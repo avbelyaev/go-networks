f2285170ebf5e5d20e77619c76f71b9b49922f2e
anthony <antonsdl@gmail.com>
2018-03-23 20:59:38 +0300

    added separate client and server

--- nw-3-peer-network/lab/peer/peer.go
+++ nw-3-peer-network/lab/peer/peer.go
@@ -5,52 +5,92 @@ import (
 	"github.com/mgutz/logxi/v1"
 	"net"
 	"encoding/json"
-	"fmt"
+	//"fmt"
 	//"strings"
 	//"strconv"
 	"os"
 	//"debug/pe"
 )
 
+type Client struct {
+	conn 		*net.TCPConn
+	enc			*json.Encoder
+	nextAddr	string
+}
+
+type Server struct {
+	conn 		*net.TCPConn
+	enc			*json.Encoder
+	selfAddr	string
+}
+
 type Peer struct {
-	logger 		log.Logger    		// Объект для печати логов
-	conn   		*net.TCPConn  		// Объект TCP-соединения
-	enc    		*json.Encoder 		// Объект для кодирования и отправки сообщений
-	selfAddr	string		  		// свой адрес, строка вида ip:port
-	nextAddr	string   			// строка вида ip:port следующего пира
+	logger 		log.Logger	// Объект для печати логов
+	client		Client
+	server 		Server
 }
 
 // create peer object
-func newPeer(conn *net.TCPConn) *Peer {
+func newPeer() *Peer {
 	return &Peer {
-		logger: log.New(fmt.Sprintf("client %s", conn.RemoteAddr().String())),
-		conn:   conn,
-		enc:    json.NewEncoder(conn),
+		logger: 	log.New("peer"),
+	}
+}
+
+// setup client for peer
+func (peer *Peer) setupConnectionFromClient(conn *net.TCPConn) {
+	peer.client = Client{
+		conn: 		conn,
+		enc: 		json.NewEncoder(conn),
+	}
+}
+
+// setup server for peer
+func (peer *Peer) setupConnectionToServer(conn *net.TCPConn) {
+	peer.server = Server{
+		conn: 		conn,
+		enc: 		json.NewEncoder(conn),
 	}
 }
 
 // start listening to incoming tcp connections
-func startServer(serverAddr string)  {
-	addr, err := net.ResolveTCPAddr("tcp", serverAddr)
+func (peer *Peer) startServer(selfAddr string)  {
+	addr, err := net.ResolveTCPAddr("tcp", selfAddr)
 	handleErr(err)
 
 	listener, err := net.ListenTCP("tcp", addr)
 	handleErr(err)
 	for {
-		incomingConnection, err := listener.A()
+		inConn, err := listener.AcceptTCP()
 		handleErr(err)
-		peer := newPeer(incomingConnection)
-		print(peer)
-		//peer.connectToNextPeer()
 
-		//go peer.handleIncomingConnection()
+		peer.setupConnectionToServer(inConn)
+
+		go peer.handleIncomingConnection()
 	}
 }
 
-// on each incoming tcp connection call this function in go routine
+
+func (peer *Peer) startClient(nextAddr string)  {
+	addr, err := net.ResolveTCPAddr("tcp", nextAddr)
+	handleErr(err)
+
+	outConn, err := net.DialTCP("tcp", nil, addr)
+	handleErr(err)
+
+	peer.setupConnectionFromClient(outConn)
+	peer.interact(outConn)
+}
+
+
+func (peer *Peer) interact(conn *net.TCPConn) {
+
+}
+
+// on each incoming tcp connection to server call this function in go routine
 func (peer *Peer) handleIncomingConnection()  {
-	defer peer.conn.Close()
-	decoder := json.NewDecoder(peer.conn)
+	defer peer.server.conn.Close()
+	decoder := json.NewDecoder(peer.client.conn)
 	for {
 		var rq Request
 		if err := decoder.Decode(&rq); err != nil {
@@ -76,7 +116,9 @@ func main() {
 	flag.StringVar(&addrStr, "addr", "127.0.0.1:6002", "specify ip address and port")
 	flag.Parse()
 
-	startServer(addrStr)
+	peer := newPeer()
+	peer.startServer("127.0.0.1:6001")
+	peer.startClient("127.0.0.1:6002")
 }
 
 
