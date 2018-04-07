6f80750c62f248f4271d192276cecbe7407d1858
anthony <antonsdl@gmail.com>
2018-04-07 03:41:16 +0300

    increased proxy dup

--- nw-4-udp/lab/proxy/proxy.sh
+++ nw-4-udp/lab/proxy/proxy.sh
@@ -6,4 +6,4 @@ export LOGXI_FORMAT=pretty,happy
 go run proxy.go -addr=127.0.0.1:5000 \
                 -server=127.0.0.1:6000 \
                 -loss=70 \
-                -dup=0
+                -dup=70
